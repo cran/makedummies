a291df71568a839170d1a15d89a64e5a6d8229e1
Toshiaki Ara <toshiaki.ara@gmail.com>
2017-01-02 17:02:35 +0000

    version 1.0

---
+++ R/makedummies.R
@@ -0,0 +1,41 @@
+makedummies <- function(dat, basal_level = FALSE, col = NULL, numerical = NULL, as.is = NULL) {
+    ## names of column and row
+    if (is.null(col)) {
+        name_col <- colnames(dat)
+    } else {
+        name_col <- unique(col)
+    }
+    name_row <- rownames(dat)
+
+    result <- NULL
+    ## process each column
+    for (name in name_col) {
+        tmp <- dat[,name]
+        if (name %in% as.is) { ## as.is option
+            res <- as.matrix(tmp)
+            colnames(res) <- name
+        } else if (is.factor(tmp)) { ## factor or ordered
+            if (name %in% numerical) { ## numerical option => convert numeric
+                res <- as.matrix(as.numeric(tmp))
+                colnames(res) <- name
+            } else { ## convert dummy variables
+                level <- levels(droplevels(tmp))
+                m <- length(tmp)
+                n <- length(level)
+                res <- matrix(0, m, n)
+                res[cbind(seq(m), tmp)] <- 1
+                colnames(res) <- paste(name, level, sep = "_")
+                ## basal_level option => delete basal level
+                if (basal_level == FALSE) {
+                    res <- res[,-1]
+                }
+            }
+        } else { ## non-factor and non-ordered => as-is
+            res <- as.matrix(tmp)
+            colnames(res) <- name
+        }
+        result <- cbind(result, res)
+    }
+    rownames(result) <- name_row
+    return(data.frame(result))
+}
